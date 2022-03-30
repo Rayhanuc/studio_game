368ae67a0adecd991982460ea6601547fb56a9be
[Rayanuc] <30935802+Rayhanuc@users.noreply.github.com>
2022-03-30 22:06:30 +0600

    code updated

--- flicks.rb
+++ flicks.rb
@@ -1,5 +1,9 @@
 puts 'Mikey loves Goonies'
-puts 'Mikey\'s favorite movie is Goonies'
+puts "Mikey's favorite movie is Goonies"
 
-movie = 'Goonies'
-puts 'Mikey\'s favorite movie is ' + movie
+movie_name = 'Goonies'
+puts "Mikey\'s favorite movie is #{movie_name}"
+
+rank = 10
+
+puts "#{movie_name} has a rank of #{rank * 2}"
