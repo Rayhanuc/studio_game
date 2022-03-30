ee1a1bca4bf4c635b0aa3439a68d669627bb8b60
[Rayanuc] <30935802+Rayhanuc@users.noreply.github.com>
2022-03-31 01:50:26 +0600

    game file added

--- flicks.rb
+++ flicks.rb
@@ -1,9 +1,20 @@
 puts 'Mikey loves Goonies'
-puts "Mikey's favorite movie is Goonies"
+puts 'Mikey\'s favorite movie is Goonies'
 
 movie_name = 'Goonies'
-puts "Mikey\'s favorite movie is #{movie_name}"
+puts 'Mikey\'s favorite movie is ' + movie_name
 
 rank = 10
 
-puts "#{movie_name} has a \trank of \n#{rank * 2}"
+puts movie_name + ' has a rank of ' + rank.to_s
+
+
+# puts 'Mikey loves Goonies'
+# puts "Mikey's favorite movie is Goonies"
+
+# movie_name = 'Goonies'
+# puts "Mikey\'s favorite movie is #{movie_name}"
+
+# rank = 10
+
+# puts "#{movie_name} has a \trank of \n#{rank * 2}"
---
+++ studio_game2.rb
@@ -0,0 +1,9 @@
+player1 = 'larry'
+player1_health = 60
+
+puts player1
+puts player1_health
+
+# puts player1 + '\'s' + ' health is ' + player1_health.to_s
+puts "#{player1}'s health is #{player1_health}"
+puts "#{player1}'s health is #{player1_health * 3}"
