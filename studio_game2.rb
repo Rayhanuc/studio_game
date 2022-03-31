82edce7e1cf173a405b3392559fbd0a0b5d45d00
[Rayanuc] <30935802+Rayhanuc@users.noreply.github.com>
2022-03-31 19:21:27 +0600

    new code added

--- studio_game2.rb
+++ studio_game2.rb
@@ -7,3 +7,12 @@ puts player1_health
 # puts player1 + '\'s' + ' health is ' + player1_health.to_s
 puts "#{player1}'s health is #{player1_health}"
 puts "#{player1}'s health is #{player1_health * 3}"
+puts "#{player1}'s health is #{player1_health / 9.0}"
+
+puts "Players: \n\tlarry \n\tcurly \n\tmoe"
+
+player_1 = "larry"
+player_2 = "curly"
+player_3 = "moe"
+
+puts "Players: \n\t#{player_1} \n\t#{player_2} \n\t#{player_3}"
