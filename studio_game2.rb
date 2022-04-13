00942fd00e82ca7cf7c7af7aea3677a4429fc850
[Rayanuc] <30935802+Rayhanuc@users.noreply.github.com>
2022-04-14 00:21:58 +0600

    4. Player #4: Shemp

--- studio_game2.rb
+++ studio_game2.rb
@@ -1,5 +1,6 @@
 player1 = 'larry'
 player1_health = 60
+player1_health = 30
 
 puts player1
 puts player1_health
@@ -15,4 +16,32 @@ player_1 = "larry"
 player_2 = "curly"
 player_3 = "moe"
 
+
+
 puts "Players: \n\t#{player_1} \n\t#{player_2} \n\t#{player_3}"
+
+# Class 4. Variables and Objects exercise
+
+# player2_health = 125
+player2_health = player1_health
+puts "#{player_1.capitalize} has a health of #{player1_health}."
+
+# puts "#{player_2.upcase} has a health of #{player2_health}."
+puts "#{player_2.upcase} has a health of #{player2_health}."
+
+player3_health = 100
+
+text = " #{player_3.capitalize} has a health of #{player3_health} "
+
+puts text.center(50, '*')
+
+player_4 = "Shemp"
+
+player4_health = 90
+
+formatted_name = player_4.capitalize.ljust(30, '.')
+
+# puts "#{formatted_name} #{player4_health} health"
+
+# 2nd mathod
+puts "#{player_4.capitalize.ljust(30, '.')} #{player4_health} health"
