ae7fbb68fe1c0fa44f822833ea27bf4a05f7ca1a
[Rayanuc] <30935802+Rayhanuc@users.noreply.github.com>
2022-04-17 21:17:00 +0600

    6. Methods
    file flicks_2.rb 6. Methods code writing has been finished

--- flicks_2.rb
+++ flicks_2.rb
@@ -1,12 +1,21 @@
-def movie_listing(title, rank=0)
-    "#{title.capitalize} has a rank of #{rank}"
+def weekday
+    current_time = Time.new
+    current_time.strftime("%A")
 end
+
+def movie_listing(title, rank=0)
+    "#{weekday.upcase}: #{title.capitalize} has a rank of #{rank} as of "
+end
+
+
 
 puts movie_listing("goonies", 10)
 puts movie_listing("shostbusters", 9)
 
 a_title = "goldfinger"
 puts movie_listing(a_title)
+
+
 
 
 
