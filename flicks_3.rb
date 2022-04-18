0d0283875ead46ec86275963b714bd0f0c386a80
[Rayanuc] <30935802+Rayhanuc@users.noreply.github.com>
2022-04-18 21:07:44 +0600

    Update flicks_3.rb
    class name 7. classes.
    video time done 11.53

--- flicks_3.rb
+++ flicks_3.rb
@@ -1,15 +1,27 @@
 class Movie
+    def initialize(title, rank)
+        @title = title
+        @the_rank = rank
+    end
+
+    def listing
+        "#{@title} has a rank of #{@the_rank}"        
+    end
 end
 
-movie1 = Movie.new
-puts movie1.object_id
+# movie1 = Movie.new
+# puts movie1.object_id
 
-movie2 = Movie.new
-puts movie2.object_id
-
-
+# movie2 = Movie.new
+# puts movie2.object_id
 
 
+movie1 = Movie.new("goonies", 10)
+# movie2 = Movie.new("ghostbusters", 9)
+
+puts movie1.listing
+movie2 = Movie.new("goonies", 10)
+puts movie2.listing
 
 
 
