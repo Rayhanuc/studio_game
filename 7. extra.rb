e458ed1765f240e633cbe5d7fb9d282ad9a586b4
[Rayanuc] <30935802+Rayhanuc@users.noreply.github.com>
2022-05-01 04:49:43 +0600

    create 7. extra.rb

---
+++ 7. extra.rb
@@ -0,0 +1,24 @@
+class Calculator
+    def initialize(a, b)
+        @a = a
+        @b = b
+    end
+
+    def add
+        @a + @b
+    end
+
+    def subtract
+        @a - @b
+    end
+
+    def multiplay
+        @a * @b
+    end
+
+    def divide
+        @a / @b
+    end
+
+    
+end
