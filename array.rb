7bf6d95e64f93337212edae9286393cfee51b855
[Rayanuc] <30935802+Rayhanuc@users.noreply.github.com>
2022-05-09 20:54:14 +0600

    array.rb
    Code updated

--- array.rb
+++ array.rb
@@ -4,6 +4,13 @@ class Movie
         @rank = rank
     end
 
+    def thumbs_up
+        @rank += 1      
+    end
+
+    def thumbs_down
+        @rank -= 1      
+    end
 
     def to_s
         "#{@title} has a rank of #{@rank}"       
@@ -16,4 +23,14 @@ movie3 = Movie.new("goonies")
 
 movies = [movie1, movie2, movie3]
 
-puts movies
+# puts movies
+
+# movies.each do |movie|
+#     movie.thumbs_up
+#     puts movie
+# end
+
+movies.each do |m|
+    m.thumbs_up
+    puts m
+end
