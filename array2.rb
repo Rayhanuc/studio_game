7e4221c722d2ad730e0609cc848c99f396c630d4
[Rayanuc] <30935802+Rayhanuc@users.noreply.github.com>
2022-06-17 00:16:36 +0600

    array2.rb repeted array

---
+++ array2.rb
@@ -0,0 +1,31 @@
+class Movie
+  def initialize(title, rank=0)
+    @title = title.capitalize
+    @rank = rank
+  end
+
+  def thumbs_up
+    @rank += 1      
+  end
+
+  def thumbs_down
+    @rank -= 1      
+  end
+
+  def to_s
+    "#{@title} has a rank of #{@rank}"       
+  end
+end
+
+movie1 = Movie.new("goonies", 10)
+movie2 = Movie.new("ghostbusters", 9)
+movie3 = Movie.new("goonies")
+
+# movies = %w(movie1 movie2 movie3)
+movies = [movie1, movie2, movie3]
+# puts movies
+
+movies.each do |m|
+  m.thumbs_up
+  puts m
+end
